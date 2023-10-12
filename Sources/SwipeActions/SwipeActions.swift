7103e6069f878aaadd868b5e99c512611b28dec5
Anton Heestand <antonnorman.heestand@rakuten.com>
2023-10-12 13:19:20 +0900

    Drag Actions

--- Sources/SwipeActions/SwipeActions.swift
+++ Sources/SwipeActions/SwipeActions.swift
@@ -1,21 +1,40 @@
 import SwiftUI
 import MultiViews
+import CoreGraphicsExtensions
 
 struct SwipeActions<Content: View>: View {
+    
+    static var dragResetAbsoluteLength: CGFloat { 100 }
+    static var dragActionAbsoluteLength: CGFloat { 100 }
+    static var dragActionRelativeFraction: CGFloat { 0.5 }
 
     let style: SwipeActionsStyle
     let leadingActions: [SwipeAction]
     let trailingActions: [SwipeAction]
     let content: () -> Content
     
+    private enum ViewState: String {
+        case inactive
+        case dragging
+        case options
+        case potentialAction
+        case action
+        var isActionable: Bool {
+            [.potentialAction, .action].contains(self)
+        }
+    }
+    @State private var viewState: ViewState = .inactive
+    
     @State private var endTimer: Timer?
 
     struct Drag {
         var isActive: Bool = false
         var translation: CGFloat = 0.0
+        var location: CGFloat = 0.0
         mutating func reset() {
             isActive = false
             translation = 0.0
+            location = 0.0
         }
     }
     @State private var drag = Drag()
@@ -23,6 +42,14 @@ struct SwipeActions<Content: View>: View {
     enum Side {
         case leading
         case trailing
+        var multiplier: CGFloat {
+            switch self {
+            case .leading:
+                return -1.0
+            case .trailing:
+                return 1.0
+            }
+        }
     }
     @State var side: Side?
     
@@ -40,19 +67,56 @@ struct SwipeActions<Content: View>: View {
         return offset
     }
     
+    private var actionOffset: CGFloat {
+        viewState.isActionable ? (
+            size.width * -(side?.multiplier ?? 0.0)
+        ) : offset
+    }
+    
     private var length: CGFloat {
         abs(offset)
     }
     
+    private var totalPaddingWidth: CGFloat {
+        min(style.padding.width * 4, length)
+    }
+    
+    private var paddingWidth: CGFloat {
+        totalPaddingWidth / 4
+    }
+    
+    private var totalLeadingSpacing: CGFloat {
+        min(style.spacing * CGFloat(leadingActions.count) * 2, length)
+    }
+    
+    private var leadingSpacing: CGFloat {
+        totalLeadingSpacing / CGFloat(leadingActions.count) / 2
+    }
+    
+    private var totalTrailingSpacing: CGFloat {
+        min(style.spacing * CGFloat(trailingActions.count) * 2, length)
+    }
+    
+    private var trailingSpacing: CGFloat {
+        totalLeadingSpacing / CGFloat(trailingActions.count) / 2
+    }
+    
+    @State var size: CGSize = .one
+    
     var body: some View {
         ZStack {
             content()
-                .offset(x: offset)
+                .offset(x: actionOffset)
+                .animation(.easeOut(duration: 0.2),
+                           value: viewState.isActionable)
             if let side: Side {
                 actionsBody(side: side)
                     .layoutPriority(-1)
             }
+            Text(viewState.rawValue)
+                .opacity(0.25)
         }
+        .readGeometry(size: $size)
         .gesture(gesture)
         .onChange(of: drag.translation) { newTranslation in
             if side != .leading, newTranslation > 0.0 {
@@ -78,31 +142,29 @@ struct SwipeActions<Content: View>: View {
                 }
             }
         }
-        .padding(.horizontal, style.padding.width)
+        .padding(.horizontal, paddingWidth)
         .padding(.vertical, style.padding.height)
     }
     
     private var leadingActionsBody: some View {
-        HStack(spacing: style.spacing) {
+        HStack(spacing: leadingSpacing) {
             ForEach(leadingActions) { action in
-                let width: CGFloat = length / CGFloat(leadingActions.count)
-                button(action: action, side: .leading, width: width)
+                button(action: action, side: .leading)
             }
         }
-        .frame(width: length)
+        .frame(width: length - paddingWidth * 2)
     }
     
     private var trailingActionsBody: some View {
-        HStack(spacing: style.spacing) {
+        HStack(spacing: trailingSpacing) {
             ForEach(trailingActions) { action in
-                let width: CGFloat = length / CGFloat(trailingActions.count)
-                button(action: action, side: .trailing, width: width)
+                button(action: action, side: .trailing)
             }
         }
-        .frame(width: length)
+        .frame(width: length - paddingWidth * 2)
     }
     
-    private func button(action: SwipeAction, side: Side, width: CGFloat) -> some View {
+    private func button(action: SwipeAction, side: Side) -> some View {
         Button(action: action.call) {
             ZStack {
                 action.style.backgroundColor
@@ -117,7 +179,7 @@ struct SwipeActions<Content: View>: View {
                 .padding(.horizontal)
                 .frame(minWidth: 0, alignment: side == .leading ? .trailing : .leading)
                 .foregroundColor(action.style.foregroundColor)
-                .frame(width: width, alignment: side == .leading ? .leading : .trailing)
+                .frame(maxWidth: .infinity, alignment: side == .leading ? .leading : .trailing)
             }
         }
         .clipShape(.rect(cornerRadius: style.cornerRadius))
@@ -128,17 +190,53 @@ struct SwipeActions<Content: View>: View {
             .onChanged { value in
                 if !drag.isActive {
                     drag.isActive = true
+                    viewState = .dragging
                     if let endTimer: Timer {
                         endTimer.invalidate()
                         self.endTimer = nil
                     }
                 }
                 drag.translation = value.translation.width
+                drag.location = value.location.x
+                
+                
+                
+                var draggingAbsoluteAction: Bool {
+                    guard let side: Side else { return false }
+                    switch side {
+                    case .leading:
+                        return drag.location > (size.width - Self.dragActionAbsoluteLength)
+                    case .trailing:
+                        return drag.location < Self.dragActionAbsoluteLength
+                    }
+                }
+                
+                var draggingRelativeAction: Bool {
+                    length > (size.width * Self.dragActionRelativeFraction)
+                }
+                
+                var draggingAction: Bool {
+                    draggingRelativeAction && draggingAbsoluteAction
+                }
+                
+                viewState = draggingAction ? .potentialAction : .dragging
             }
             .onEnded { value in
+                
+                let isAtStart: Bool = abs(value.translation.width) < Self.dragResetAbsoluteLength
+                
+                if viewState == .potentialAction {
+                    viewState = .action
+                } else if isAtStart {
+                    viewState = .inactive
+                } else {
+                    viewState = .options
+                }
+                
                 withAnimation(.easeInOut(duration: 0.5)) {
                     drag.translation = 0.0
                 }
+                
                 endTimer = Timer.scheduledTimer(withTimeInterval: 0.5, repeats: false) { _ in
                     drag.reset()
                     side = nil
